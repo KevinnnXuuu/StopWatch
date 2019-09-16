d89d2bbbe505e207c09d700e343c99e6529f8fad
kevinxuuuu <ye627627@gmail.com>
2019-09-16 13:39:04 -0700

    Finished

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.stopwatch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,17 +9,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonStartStop;
+    private ToggleButton buttonStartStop;
     private Button buttonReset;
     private Chronometer chronometerTime;
     public static final String TAG = MainActivity.class.getSimpleName();
-    private long elapsedms;
-
+    public static final String KEY_CHRONOMETER_BASE = "chronometer base";
+    public static final String KEY_CHRONOMETER_STOPTIME = "chronometer stop time";
+    public static final String KEY_CHRONOMETER_STARTTIME = "chronometer start time";
+    public static final String KEY_CHRONOMETER_STATE = "chronometer state";
+    public boolean state;
     // Look up the log class for Android.
     // list all the levels of logging and when they're used
     // v
@@ -42,9 +48,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: ");
-
         wireWidgets();
         setLitseners();
+
+        // if the savedInstanceState isn't null
+            // pull out the value of the base that we saved from the Bundle
+            // set the chronometer's base to that value
+            // start the chronometer
+        if (savedInstanceState != null) {
+            long baseBundle = savedInstanceState.getLong(KEY_CHRONOMETER_BASE);
+            chronometerTime.setBase(baseBundle);
+            long stopTime = savedInstanceState.getLong(KEY_CHRONOMETER_STOPTIME);
+            boolean state = savedInstanceState.getBoolean(KEY_CHRONOMETER_STATE);
+            if (state) {
+                chronometerTime.stop();
+            }
+            if (!state) {
+                chronometerTime.stop();
+                chronometerTime.setBase(chronometerTime.getBase() + SystemClock.elapsedRealtime() - stopTime);
+            }
+
+        }
+
+
+
+        // next functionally would be to also store in the bundle
+        // whether it was running or stopped to decide if you should
+        // start it or not in onCreate
     }
 
     @Override
@@ -59,7 +89,6 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         Log.d(TAG, "onResume: ");
     }
-
     // activity is now running
 
     // another activity is covering part od this activity
@@ -76,6 +105,15 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onStop: ");
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong(KEY_CHRONOMETER_BASE, chronometerTime.getBase());
+        outState.putLong(KEY_CHRONOMETER_STOPTIME, stopTime);
+        outState.putLong(KEY_CHRONOMETER_STARTTIME, startTime);
+        outState.putBoolean(KEY_CHRONOMETER_STATE, state);
+    }
+
     // when activity is finished
     @Override
     protected void onDestroy() {
@@ -90,35 +128,34 @@ public class MainActivity extends AppCompatActivity {
     }
 
     long startTime = SystemClock.elapsedRealtime();
+    long stopTime = SystemClock.elapsedRealtime();
     public void setLitseners(){
-
-        buttonStartStop.setOnClickListener(new View.OnClickListener() {
+        buttonStartStop.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View view) {
-                if (buttonStartStop.getText().equals("STOP")) {
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (buttonStartStop.getText().toString().equals(getString(R.string.main_stop))) {
                     chronometerTime.stop();
-                    startTime = SystemClock.elapsedRealtime();
-                    buttonStartStop.setText("START");
+                    state = false;
+                    stopTime = SystemClock.elapsedRealtime();
+                    buttonStartStop.setText(getString(R.string.main_start));
                 }
                 else {
-                    chronometerTime.setBase(chronometerTime.getBase() + SystemClock.elapsedRealtime() - startTime);
+                    chronometerTime.setBase(chronometerTime.getBase() + SystemClock.elapsedRealtime() - stopTime);
                     chronometerTime.start();
-                    buttonStartStop.setText("STOP");
-
+                    state = true;
+                    buttonStartStop.setText(getString(R.string.main_stop));
                 }
             }
         });
-
         buttonReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                buttonStartStop.toggle();
                 chronometerTime.stop();
                 chronometerTime.setBase(SystemClock.elapsedRealtime());
                 startTime = SystemClock.elapsedRealtime();
-                buttonStartStop.setText("START");
+                buttonStartStop.setText(getString(R.string.main_start));
             }
         });
     }
 }
-
-
